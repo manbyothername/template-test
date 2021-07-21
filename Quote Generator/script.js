94e0e9514a11e6ede9381317ff1bea84188ca5e9
Osvaldo Rodriguez <osvaldo.gabrielrc@outlook.com>
2021-07-21 15:10:12 -0400

    new quote javascript

---
+++ Quote Generator/script.js
@@ -0,0 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+//New Quote
+function newQuote(){
+    //Random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+}
+
+//Get Quotes fom API
+async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error){
+        //Error here
+    }
+}
+//On load
+getQuotes();
